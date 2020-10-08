dee54bb43b702bd8c347aec4cd0d1b2fa5c5ada6
serknight <chrisknight.mail@gmail.com>
2020-10-08 12:53:04 -0600

    Heroku

--- src/models/index.ts
+++ src/models/index.ts
@@ -7,10 +7,8 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
     logging:  true //false
-  })
+  });
 } else {
   sequelize = new Sequelize(
     process.env.DATABASE,
